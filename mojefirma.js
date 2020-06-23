64b5397754c64ec02f4456575f3da7fa8e540836
TaraJura <63288402+TaraJura@users.noreply.github.com>
2020-06-23 09:58:18 +0200

    web

---
+++ mojefirma.js
@@ -0,0 +1,12 @@
+
+
+
+function button(){
+document.getElementById("regB").innerHTML = "Stačí tak málo" ;
+
+}
+
+function button1(){
+document.getElementById("regB").innerHTML = "Registrovat" ;
+
+}
---
+++ site.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html:
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+
+
+    <form action= "site.php" method="get">
+
+
+        <input type="submit">
+    </form>
+<br><br>
+
+
+    <?php ?>
+
+</body>
+</html>
+
+
+
+
